77763ed1c109216a75dad10dc04eb43b746b89f1
codewitharshad <developer.codewitharsh@gmail.com>
2023-09-10 02:25:34 +0530

    Adding pages sample and all dependency installed

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import { useEffect } from 'react';
+import useSWR from 'swr'
+
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    import("bootstrap/dist/css/bootstrap.css")
+    import("bootstrap/dist/js/bootstrap.js")
+  }, []);
   return <Component {...pageProps} />
 }
---
+++ src/pages/auth/login.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from "@/styles/auth/Login.module.css"
+const login = () => {
+  return (
+    <div>
+        
+    </div>
+  )
+}
+
+export default login
