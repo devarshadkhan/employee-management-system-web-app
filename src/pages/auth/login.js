fa076e852baf59ed088ebb536cb703d021f7463e
codewitharshad <developer.codewitharsh@gmail.com>
2023-09-14 02:38:42 +0530

    Auth done with validation

--- src/pages/auth/login.js
+++ src/pages/auth/login.js
@@ -2,29 +2,70 @@ import React, { useState } from "react";
 import styles from "@/styles/auth/Login.module.css";
 import axios from "axios";
 import classNames from "classnames";
+import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { ToastContainer, toast } from "react-toastify";
+import { useRouter } from "next/router";
 const login = () => {
+  const router = useRouter()
   const [email, setEmail] = useState();
   const [password, setPassword] = useState();
-  const login_User = async () => {
-    const res = await axios.post("https://emp-api-v2.onrender.com/auth/login", {
-      email: email,
-      password: password
-    });
-    localStorage.setItem("token",res.data.token)
-    console.log(res.data.token);
+  const {
+    register,
+    setValue,
+    getValues,
+    trigger,
+    reset,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ mode: "onChange", reValidateMode: "onChange" });
+  const login_User = async (data) => {
+    await axios.post("https://emp-api-v2.onrender.com/auth/login",data)
+    .then((res)=>{
+      if(res){
+        localStorage.setItem("token",res.data.token)
+       router.push("/")
+      }
+    })
+    .catch((error)=>{
+      console.log(error);
+      toast(error.response ? error.response.data.message : error.message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "dark",
+      });
+    })
   };
   return (
     <div>
+    <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
       <div className={styles.login_wrp}>
         <div className="container">
           <div className="row">
             <div className="col-12">
               <h1>Account Login</h1>
               <p>
-                If you are already a member you can login with <span>your email
+                If you are already a member you can login with <span className="dsadaas">your email
                 address and password.</span>
               </p>
-              <form>
+              <form onSubmit={handleSubmit(login_User)}>
                 <div className={styles.login}>
                   <p>Email address</p>
                   <div class="form-floating mb-3">
@@ -33,10 +74,22 @@ const login = () => {
                       className={classNames("form-control",styles.inputs )}
                       id="floatingInput"
                       placeholder="name@example.com"
-                      value={email}
-                      onChange={(e) => setEmail(e.target.value)}
+                      // value={email}
+                      // onChange={(e) => setEmail(e.target.value)}
+                      {...register("email", {
+                        required: "Please enter valid Email Address",
+                        pattern: {
+                          value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                          message: "Please enter a valid Email Address",
+                        },
+                      })}
                     />
                     <label for="floatingInput">Email address</label>
+                    {errors.email && (
+                      <p role="alert" style={{ color: "red" }}>
+                        {errors.email.message}
+                      </p>
+                    )}
                   </div>
                   <p>Password</p>
                   <div class="form-floating">
@@ -45,16 +98,29 @@ const login = () => {
                        className={classNames("form-control",styles.inputs )}
                       id="floatingPassword"
                       placeholder="Password"
-                      value={password}
-                      onChange={(e) => setPassword(e.target.value)}
+                      // value={password}
+                      // onChange={(e) => setPassword(e.target.value)}
+                      {...register("password", {
+                        required: "Password is required",
+                      })}
                     />
                     <label for="floatingPassword">Password</label>
+                    {errors.password && (
+                      <p role="alert" style={{ color: "red" }}>
+                        {errors.password.message}
+                      </p>
+                    )}
                   </div>
 
-                  <button type="button" onClick={login_User}>Login</button>
+                  <button type="submit" >Login</button>
                 </div>
               </form>
             </div>
+              <div className="col-12">
+                <div className={styles.resg}>
+                <h6 className={styles.acc}>Dont have an account ? <Link href={"/auth/register"} className={styles.asspan}>Sign up here</Link> </h6>
+                </div>
+              </div>
           </div>
         </div>
       </div>
